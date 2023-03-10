f23508a0e8c28e4b99e7bb07de51b9b7ec6fff73
Pierre Payet <pierre.payet@outlook.com>
2023-03-10 16:37:52 +0100

    feat: create Form.vue component and apartment-[slug].vue page, add apartment images, create IApartment interface

---
+++ front-end/pages/apartment-[slug].vue
@@ -0,0 +1,18 @@
+<template>
+  <Apartment
+    :title="apartment.title"
+    :imageSrc="apartment.imageSrc"
+    :imageAlt="apartment.imageAlt"
+    :price="apartment.price"
+    :address="apartment.address"
+    :numberOfParkingSpaces="apartment.numberOfParkingSpaces"
+    :numberOfBathrooms="apartment.numberOfBathrooms"
+    :numberOfBedrooms="apartment.numberOfBedrooms"
+    :isFeatured="apartment.isFeatured"
+    :isCondominium="apartment.isCondominium"
+    :stateOfFurniture="apartment.stateOfFurniture"
+    :area="apartment.area"
+    :pricePerSquareMeter="apartment.pricePerSquareMeter"
+  ></Apartment>
+  <Form></Form>
+</template>
---
+++ front-end/types/index.ts
@@ -0,0 +1,15 @@
+export interface IApartment {
+  title: string;
+  imageSrc: string;
+  imageAlt: string;
+  price: number;
+  address: string;
+  numberOfParkingSpaces: number;
+  numberOfBathrooms: number;
+  numberOfBedrooms: number;
+  isFeatured: boolean;
+  isCondominium: boolean;
+  stateOfFurniture: string;
+  area: number;
+  pricePerSquareMeter: number;
+}
